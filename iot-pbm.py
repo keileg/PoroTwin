5dfe98e98b70c2c5f262616ae99be827976f3743
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2022-03-28 10:55:19 +0200

    Update iot-pbm to new Costa protocol

--- iot-pbm.py
+++ iot-pbm.py
@@ -6,11 +6,14 @@ from parabolic_model import ParabolicSolver
 
 def main():
     pbm = ParabolicSolver(known_solution=False)
+
+    name = "parabolic_porepy"
+
     with open("config.json", "r") as f:
         config = json.load(f)
         cstr = config["COSTA_PBM_CSTR"]
 
-    with PbmServer(cstr, pbm) as server:
+    with PbmServer(name, cstr, pbm) as server:
         server.wait()
 
 
