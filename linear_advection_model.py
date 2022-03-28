75ae1ff3ae5ecfb62e5d252f242f6249ada9f986
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2022-03-28 10:57:32 +0200

    Linear advection model for two-well case

--- linear_advection_model.py
+++ linear_advection_model.py
@@ -9,6 +9,8 @@ import scipy.sparse.linalg as spla
 import scipy.sparse as sps
 from Costa.api import PhysicsModel
 
+from parabolic_model import ParabolicSolver
+
 
 Parameters = Dict[str, Union[float, List[float]]]
 Vector = np.ndarray
@@ -29,10 +31,11 @@ class LinearAdvection(PhysicsModel):
 
     """
 
-    def __init__(self) -> None:
+    def __init__(self, config: Dict) -> None:
         self.variable = "u"
 
         self.parameter_key = "transport"
+        self._config = config
 
         self._is_discretized = False
         self._set_model()
@@ -42,20 +45,24 @@ class LinearAdvection(PhysicsModel):
         self._set_parameters()
         self._set_equations()
 
-    def create_grid(self) -> None:
+    def create_grid(self):
+        # Set the simulation model, including parameters.
+        # In the future, this information should be read from an input file
+
         # Grid size and resolution
         dim = 2
 
-        Nx = [64, 64]
-        phys_dims = np.array([1, 1])
+        Nx = self._config["Nx"]
+        phys_dims = self._config.get("phys_dims", np.ones(2))
+
         g = pp.CartGrid(Nx, phys_dims)
-        g.nodes[:2] -= 0.5
         g.compute_geometry()
 
         # Convert to mixed-dimensional grid (this is a bit of an overkill
         # for this problem, but it works).
         gb = pp.GridBucket()
         gb.add_nodes([g])
+
         self.dim = dim
         self.gb = gb
 
@@ -63,6 +70,7 @@ class LinearAdvection(PhysicsModel):
         for g, d in self.gb:
 
             dir_faces = np.where(g.tags["domain_boundary_faces"])[0]
+            dir_faces = np.array([])
 
             bc_type = dir_faces.size * ["dir"]
             bc = pp.BoundaryCondition(g, faces=dir_faces, cond=bc_type)
@@ -74,6 +82,24 @@ class LinearAdvection(PhysicsModel):
             velocity = np.vstack((-4 * xf[1], 4 * xf[0]))
 
             fluxes = np.sum(g.face_normals[:2] * velocity, axis=0)
+            fluxes[:] = 0
+
+            injection = np.zeros(g.num_cells)
+            production = np.zeros(g.num_cells)
+            if "injection" in self._config:
+
+                injection_cell = g.closest_cell(self._config["injection"])
+                injection_1d = np.ravel_multi_index(
+                    (injection_cell), self._config["Nx"]
+                )
+                injection[injection_1d] = 1
+
+            if "production" in self._config:
+                production_cell = g.closest_cell(self._config["production"])
+                production_1d = np.ravel_multi_index(
+                    (production_cell), self._config["Nx"]
+                )
+                production[production_1d] = -1
 
             d[pp.PRIMARY_VARIABLES] = {self.variable: {"cells": 1}}
 
@@ -83,6 +109,8 @@ class LinearAdvection(PhysicsModel):
                     "bc_values": bc_values,
                     "mass_weight": mass_weight,
                     "darcy_flux": fluxes,
+                    "injection_source": injection,
+                    "production_source": production,
                 }
             }
             # Initial values - these will be overridden by the Costa-related methods
@@ -119,8 +147,6 @@ class LinearAdvection(PhysicsModel):
         grids = [g]
         U = eq_manager.variable(g, self.variable)
 
-        U_prev = U.previous_timestep()
-
         advection = pp.ad.UpwindAd(self.parameter_key, grids)
         div = pp.ad.Divergence(grids)
         mass = pp.ad.MassMatrixAd(self.parameter_key, grids)
@@ -131,12 +157,21 @@ class LinearAdvection(PhysicsModel):
         flux_mat = sps.dia_matrix((flux, 0), (g.num_faces, g.num_faces))
         flux_ad = pp.ad.Matrix(flux_mat, name="flux_scaling")
 
+        injection = gb.node_props(g, pp.PARAMETERS)[self.parameter_key][
+            "injection_source"
+        ]
+        production = gb.node_props(g, pp.PARAMETERS)[self.parameter_key][
+            "production_source"
+        ]
+
         self._eq_comp = {
             "mass": mass.mass,
             "div": div,
             "advection": advection,
             "flux": flux_ad,
             "bc": bc,
+            "injection": injection,
+            "production": production,
             self.variable: U,
         }
         self._dof_manager = dof_manager
@@ -173,7 +208,16 @@ class LinearAdvection(PhysicsModel):
         U_prev = U.previous_timestep()
 
         source_ad = pp.ad.Array(source_given)
-        eq = div * (flux * (adv.upwind * U) + adv.bound_transport_dir * bc) + source_ad
+
+        injection = pp.ad.Array(self._eq_comp["injection"])
+        production = pp.ad.Array(self._eq_comp["production"]) * U
+
+        eq = (
+            div * (flux * (adv.upwind * U) + adv.bound_transport_dir * bc)
+            + source_ad
+            + injection
+            + production
+        )
 
         eq += mass * (U - U_prev) / dt
 
@@ -245,16 +289,12 @@ class LinearAdvection(PhysicsModel):
 class LinearAdvectionWithGravity(LinearAdvection):
     """Linear advection model, but with a tracer which is heavier than the ambient fluid."""
 
-    def __init__(self, params: Dict = None):
+    def __init__(self, config: Dict = None):
         self.gravity_parameter_key = "gravity"
-        super().__init__()
+        super().__init__(config)
 
-        if params is None:
-            params = {}
-
-        self.params = {}
-        self.params["rho_1"] = params.get("rho_1", 1)
-        self.params["rho_2"] = params.get("rho_2", 1)
+        self._config["rho_1"] = config.get("rho_1", 1)
+        self._config["rho_2"] = config.get("rho_2", 1)
 
     def _set_parameters(self) -> None:
         super()._set_parameters()
@@ -306,11 +346,11 @@ class LinearAdvectionWithGravity(LinearAdvection):
         source_ad = pp.ad.Array(source_given)
 
         # Advective flux, for the circular field
-        advective_flux = flux * (adv.upwind * U) + adv.bound_transport_dir * bc
+        advective_flux = flux * (adv.upwind * U)  # + adv.bound_transport_dir * bc
 
         # Gravity term, which will drag the plume downwards
-        rho_1 = self.params["rho_1"]
-        rho_2 = self.params["rho_2"]
+        rho_1 = self._config["rho_1"]
+        rho_2 = self._config["rho_2"]
 
         grav_adv = pp.ad.UpwindAd(self.gravity_parameter_key, grids=[g])
         non_linear_upwind = grav_adv.upwind * (U * U - U)
@@ -320,9 +360,13 @@ class LinearAdvectionWithGravity(LinearAdvection):
             * (rho_2 - rho_1)
             * pp.GRAVITY_ACCELERATION
             * vertical_face_components
+            * 0
         )
 
-        eq = div * (advective_flux + gravity_term) + source_ad
+        injection = pp.ad.Array(self._eq_comp["injection"])
+        production = pp.ad.Array(self._eq_comp["production"]) * U
+
+        eq = div * (advective_flux) + source_ad - (injection + production)
 
         eq += mass * (U - U_prev) / dt
 
@@ -386,65 +430,139 @@ class LinearAdvectionWithGravity(LinearAdvection):
         """
         return self._solve(params=params, uprev=uprev)
 
-    def residual(self, params: Parameters, uprev: Vector, unext: Vector) -> Vector:
-        """Calculate the residual b - Au given the assumed solution unext.
-        :param params: Dictionary of parameters.
-            By convention the timestep is named 'dt'.
-        :param uprev: Previous timestep.  May be ignored by a stationary solver.
-        :param unext: The purported exact or experimental solution.
-        :return: The residual b - Au."""
-        # Assemble linearized system, return residual vector.
-        _, b = self._assemble(params=params, p_prev=uprev, p_now=unext)
-        return b
 
-    def correct(self, params, uprev: Vector, sigma: Vector) -> Vector:
-        """Calculate a corrected prediction of the next timestep given
-        the previous timestep and a right-hand side perturbation.
-        :param params: List of parameters.  By convention the first parameter is
-            the timestep.
-        :param uprev: Previous timestep.  May be ignored by a stationary solver.
-        :param sigma: Right-hand-side perturbation. If equal to zero, this method
-            should be equivalent to predict(params, uprev).
-        :return: Corrected prediction of next timestep.
-        """
+class LinearAdvectionTwoWells(LinearAdvectionWithGravity):
+    def __init__(self, config):
 
-        # Assemble, solve and return
-        return self._solve(params=params, uprev=uprev, source=sigma)
+        config.update({"rho_1": 1, "rho_2": 1})
+
+        super().__init__(config)
+
+        self._set_model()
+
+    def _set_model(self) -> None:
+        self.create_grid()
+
+        self._set_transport_parameters()
+
+        g = self.gb.grids_of_dimension(self.dim)[0]
+
+        bound_faces = g.tags["domain_boundary_faces"]
+
+        dir_faces = np.where(
+            np.abs(g.face_centers[1, bound_faces] - self._config["phys_dims"][1]) < 1e-5
+        )[0]
+        self._config["dir_faces"] = np.array(dir_faces)
+
+        injection_cell = g.closest_cell(self._config["injection"])
+        production_cell = g.closest_cell(self._config["production"])
+
+        injection_1d = np.ravel_multi_index((injection_cell), self._config["Nx"])
+        production_1d = np.ravel_multi_index((production_cell), self._config["Nx"])
+
+        source = np.zeros(g.num_cells)
+        source[injection_1d] = 1
+        source[production_1d] = -1
+
+        prev_sol = np.zeros_like(source)
+
+        # Construct Parabolic solver, which will override the set advection field
+        flow_model = ParabolicSolver(False, self._config)
+        flow_model.correct(
+            {"dt": 1.0, "t": 0.0, "ALPHA": 0.0}, uprev=prev_sol, sigma=source
+        )
+
+        g_flow = flow_model.gb.grids_of_dimension(2)[0]
+        flux = flow_model.gb.node_props(g_flow, pp.PARAMETERS)[
+            flow_model.flow_parameter_key
+        ]["darcy_flux"].copy()
+        self.pressure = flow_model.gb.node_props(g_flow, pp.STATE)[flow_model.variable]
+        self.gb.node_props(g, pp.PARAMETERS)[self.parameter_key]["darcy_flux"] = flux
+        self._set_transport_equation()
+
+    def _set_transport_parameters(self):
+        super()._set_parameters()
+
+    def _set_transport_equation(self):
+        super()._set_equations()
+
+    def _initial_condition(self, g):
+
+        return np.zeros(g.num_cells)
 
 
 if __name__ == "__main__":
-    T = np.pi / 2
-    n_steps = 100
-    params = {"dt": T / n_steps}
-    model_no_gravity = LinearAdvectionWithGravity(params={"rho_1": 1, "rho_2": 1})
-    model_gravity = LinearAdvectionWithGravity(params={"rho_1": 1, "rho_2": 1.03})
 
-    U_no_gravity = model_no_gravity._initial_condition(
-        model_no_gravity.gb.grids_of_dimension(2)[0]
-    )
-    U_gravity = U_no_gravity.copy()
+    if True:
+        model = LinearAdvectionTwoWells(
+            {
+                "Nx": [50, 50],
+                "phys_dims": [1, 1],
+                "injection": [1 / 4, 1 / 3],
+                "production": [3 / 4, 1 / 3],
+            }
+        )
 
-    model_e = LinearAdvection()
+        exp = pp.Exporter(model.gb, "two_wells", folder_name="tmp_viz")
 
-    exp = pp.Exporter(model_e.gb, "gravity", folder_name="tmp_viz")
-    time_steps = []
+        time_steps = [0]
 
-    for i in range(n_steps):
-        Up_ng = U_no_gravity.copy()
-        U_no_gravity = model_no_gravity.predict(params, uprev=Up_ng)
+        T = 0.5
+        n_steps = 100
+        params = {"dt": T / n_steps}
 
-        Up_g = U_gravity.copy()
-        U_gravity = model_gravity.predict(params, uprev=Up_g)
+        U = model._initial_condition(model.gb.grids_of_dimension(2)[0])
+        exp.write_vtu(["U"], time_dependent=True, time_step=0)
 
-        if i % 10 == 0:
-            print(f"Time step {i}")
-            gb = model_e.gb
-            g = model_e.gb.grids_of_dimension(2)[0]
-            state = gb.node_props(g, pp.STATE)
-            state["no_grav"] = U_no_gravity
-            state["grav"] = U_gravity
-            exp.write_vtu(["no_grav", "grav"], time_dependent=True, time_step=i)
+        for i in range(n_steps):
+            Up = U.copy()
+            U = model.predict(params, uprev=Up)
 
-            time_steps.append(i)
+            if i > 0 and i % 5 == 0:
+                print(f"Time step {i}")
+                gb = model.gb
+                g = model.gb.grids_of_dimension(2)[0]
+                state = gb.node_props(g, pp.STATE)
+                state["U"] = U
+                exp.write_vtu(["U"], time_dependent=True, time_step=i)
 
-    exp.write_pvd(time_steps)
+                time_steps.append(i)
+
+        exp.write_pvd(time_steps)
+
+    if False:
+        T = 0.1
+        n_steps = 100
+        params = {"dt": T / n_steps}
+        model_no_gravity = LinearAdvectionWithGravity(params={"rho_1": 1, "rho_2": 1})
+        model_gravity = LinearAdvectionWithGravity(params={"rho_1": 1, "rho_2": 1.03})
+
+        U_no_gravity = model_no_gravity._initial_condition(
+            model_no_gravity.gb.grids_of_dimension(2)[0]
+        )
+        U_gravity = U_no_gravity.copy()
+
+        model_e = LinearAdvection()
+
+        exp = pp.Exporter(model_e.gb, "gravity", folder_name="tmp_viz")
+        time_steps = []
+
+        for i in range(n_steps):
+            Up_ng = U_no_gravity.copy()
+            U_no_gravity = model_no_gravity.predict(params, uprev=Up_ng)
+
+            Up_g = U_gravity.copy()
+            U_gravity = model_gravity.predict(params, uprev=Up_g)
+
+            if i % 10 == 0:
+                print(f"Time step {i}")
+                gb = model_e.gb
+                g = model_e.gb.grids_of_dimension(2)[0]
+                state = gb.node_props(g, pp.STATE)
+                state["no_grav"] = U_no_gravity
+                state["grav"] = U_gravity
+                exp.write_vtu(["no_grav", "grav"], time_dependent=True, time_step=i)
+
+                time_steps.append(i)
+
+        exp.write_pvd(time_steps)
